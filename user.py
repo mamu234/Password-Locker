12ed7b68648a42e9a7c29d87360e0bfd53489474
carolyne <pcmaunda@gmail.com>
2022-02-19 21:23:49 +0300

    added user method

---
+++ user.py
@@ -0,0 +1,21 @@
+class User:
+"""
+generate new instance of contact
+
+"""
+    usersList =[]
+
+    def __init__ (self,firstname, lastname,password)
+
+    """
+the init method  defines properties of our object
+    """
+
+    self.firstname = firstname
+    self.lastname =lastname
+    self.password = password
+
+def __init__user(self):
+    """
+thsi method saves user information to the userslist
+    """
