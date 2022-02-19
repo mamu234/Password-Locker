da10f36d77632b20b5f59c724bcf29f026562730
carolyne <pcmaunda@gmail.com>
2022-02-19 21:34:52 +0300

    added class method

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class User:
 generate new instance of contact
 
 """
-    usersList =[]
+    userslist =[]
 
     def __init__ (self,firstname, lastname,password)
 
@@ -15,7 +15,16 @@ the init method  defines properties of our object
     self.lastname =lastname
     self.password = password
 
-def __init__user(self):
+def save_user(self):
     """
-thsi method saves user information to the userslist
+this method saves user information to the userslist
     """
+
+       User.userslist.append(self)
+
+def delete_user(self):
+    """
+
+    the delete  method removes user from th userlist
+    """
+    
