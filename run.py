c8cdeb3c4d3f130a22d10cdc5fe73b7210ee8f61
carolyne <pcmaunda@gmail.com>
2022-02-21 13:35:29 +0300

    modified runpy file

--- run.py
+++ run.py
@@ -19,7 +19,16 @@ def find_user(number):
 def display_users():
     return User.display_users()
 
-def create_account
+def create_account(accountusername,accountname,accountpassword):
+    newaccount=Credentials(accountusername,accountname,accountpassword)
+    return newaccount
+def save_account(user):
+    user.save_account()
+def delete_account()
+def find_account(number):
+    return Credentials.find_by_number(number)
+def display_accounts():
+    
 
 def main():
         while True:
