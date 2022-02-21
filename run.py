6e484e535721d33df3aa74b891d835e317fc5893
carolyne <pcmaunda@gmail.com>
2022-02-21 12:33:31 +0300

    added credential class

--- run.py
+++ run.py
@@ -1,91 +1,33 @@
-import random
+
+import pyperclip
 from user import User
+/usr/bin/env python3.6
+
+def create_user(firstname,lastname,username,userpassword):
+    newuser= User(firstname,lastname,username,userpassword)
+    return newuser
+
+def save_user(user):
+    user.save_user()
+
+def delete_user(user):
+    user.delete_user()
+def find_user(number):
+    return User.find_by_number(number)
+def display_users():
+    return User.display_users()
 
 def main():
+        while True:
+            print("welcome to password Vault write SU or LG to start")
+            print("SU -or LG")
+            option-input()
+            if option == "SU":
+                print("create Account")
+                print("-")     
 
-     while True:
-         print("welcome to pssword locker!!")
-         print('\n')
-         print("choose a short code to creae new user  using 'nu' to login in the account 'lg' or 'ex' to exit")
-         short_code = input().lower()
-         print('\n')
-
-         if short_code == 'nu'
-             print('create username')
-             created_user_name = input()
-
-             print('create password')
-             created_password = input()
-
-             while confrim_password != created_user_password:
-                 print("invalid password  did nto match")
-                 print("enter your password")
-                 created_user_password = input()
-                 print("confirm your password")
-                 confrim_password = input()
-
-             else:
-                 print(f"congratualtions{created_user_name}! account successfully created")
-                 print('\n')
-                 print("process to login")
-                 print("Username")
-                 entred_username = input()
-                 print("your password")
-                 entered_password = input()
-
-             while entred_username!= created_user_name or entered_password !=created_user_password:
-                 print("invalid username or password")
-                 print("username")
-                 entred_username = input()
-                 print("your password")
-                 entered_password = input()
-
-            else:
-                  print(f"congratulations{created_user_name} ! account created successfuly")
-                  print('\n')
-                  print("proced to login")
-                  print("Username")
-                  entered_username = input()
-                  print("your password")
-                  entered_password = input()
-
-              else:
-                  print(f"welcome: {entered_username} to your account")
-                  print('\n')
-
-
-              elif short_code == 'lg':
-                  print("welcome")
-                  print("enter user name")
-                  default_user_name = input()
-
-                  print("enter password")
-                  default_user_password = input()
-                  print('\n')
-                  while default_user_name != 'testuser' or default_user_password != '09876'
-                  print("wrong userName or password.Username"  'testuer' and password '09876')
-                  print("enter user name")
-                  default_user_name = input()
-
-
-                  print("enter password")
-                  default_user_password = input()
-                  print('\n')
-
-              else:
-                  print("login sucess")
-                  print('\n')
-                  print('\n')
-
-
-               elif short_code == 'ex':
-                   break
-               else:
-                   print("enter vslid code to continue")
-
-        
- if __name__ == '__main__':
-     main()
+if __name__ == '__main__':
+    main()
 
 
 
--- user.py
+++ user.py
@@ -19,3 +19,33 @@ class User :
 
          """     
          User.user_list.append(self)
+   
+
+   def delete_user(self):
+         """
+         delete method deletes saved user from the userlist
+         """
+        User.userlist.remove(self)
+    @classmethod
+    def display_users(cls):
+        """
+        display method return info from the userlist
+        """
+        
+        return cls.userlist
+    @classmethod
+    def find_by_number(cls,number):
+        """
+        find by number method takes a username and return the user  if its matches the number
+        """
+
+        for user in cls.userlist:
+            if user.password == number:
+                return user
+
+    @classmethod
+    def user_exist(cls,number):
+        for user in cls.userlist:
+            if user.username == number:
+                return True
+                return False
